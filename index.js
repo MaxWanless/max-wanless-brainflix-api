c7433c56d4310444377754aca0b327135c407987
Max Wanless <maxwanless@gmail.com>
2022-08-29 18:16:46 -0400

    Added Cors

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 require("dotenv").config();
 const videosRoute = require("./routes/videos");
 const { PORT, BACKEND_URL } = process.env;
+const cors = require("cors");
+
+//Cors middleware
+app.use(cors());
 
 // required to accces the body
 app.use(express.json());
 
 app.use("/videos", videosRoute);
-
 
 app.listen(8080, () => {
   console.log("Running");
