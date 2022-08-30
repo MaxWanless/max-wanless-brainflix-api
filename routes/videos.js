2a82f8225e35b8e7c57dae067cd9c9fb59ed02a3
Max Wanless <maxwanless@gmail.com>
2022-08-29 21:14:13 -0400

    Connected Video upload front end to back end

--- routes/videos.js
+++ routes/videos.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const { response } = require("express");
-const { error } = require("console");
 const { v4: uuidv4 } = require("uuid");
 
 // Return Video List endpoint
@@ -30,26 +28,27 @@ router.get("/:id", (req, res) => {
   }
 });
 
-//Static images endpoint
-
-
-// Video upload endpoint
+// Video Post endpoint
 router.post("/newVideo", (req, res) => {
   console.log("hit");
   let videosDetailList = JSON.parse(
     fs.readFileSync("./data/video-details.json")
   );
-
+  console.log(req.body.description)
   let newVideo = {
     title: req.body.title,
     channel: "Bing Bong",
     image: "http://localhost:8080/endpoint-files/Upload-video-preview.jpg",
     description: req.body.description,
-    views: Math.floor(Math.random() * 1000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-    likes: Math.floor(Math.random() * 1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    views: Math.floor(Math.random() * 1000000)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    likes: Math.floor(Math.random() * 1000)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
     duration: "3:02",
     video: "https://project-2-api.herokuapp.com/stream",
-    timestamp:  Date.now(),
+    timestamp: Date.now(),
     comments: [],
     id: uuidv4(),
   };
